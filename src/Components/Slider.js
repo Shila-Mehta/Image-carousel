dbc18529368d6205521bffe81d7f61fd1e33bc18
Nimra Jabbar <nimraabduljabbar14@gmail.com>
2025-06-09 13:08:56 +0500

    first commit

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -1,21 +1,21 @@
    export class Slider {
     constructor() {
         const slide1 = [
-            "../Assets/Duck1.jpg",
-            "../Assets/Duck2.jpg",
-            "../Assets/Duck3.jpg"        
+            "Assets/Duck1.jpg",
+            "Assets/Duck2.jpg",
+            "Assets/Duck3.jpg"        
             ];
 
         const slide2 = [
-            "../Assets/sheep1.jpg",
-            "../Assets/sheep2.jpg",
-            "../Assets/sheep3.jpg"
+            "Assets/sheep1.jpg",
+            "Assets/sheep2.jpg",
+            "Assets/sheep3.jpg"
         ];
 
         const slide3 = [
-             "../Assets/cat1.jpg",
-             "../Assets/cat2.jpg",
-             "../Assets/cat3.jpg",
+             "Assets/cat1.jpg",
+             "Assets/cat2.jpg",
+             "Assets/cat3.jpg",
 
         ];
 
